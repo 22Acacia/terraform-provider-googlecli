705677f9a8169c28e5939663b3d412ca9f7e151b
pat <pat@22acacia.com>
2015-11-28 03:08:35 -0800

    cleanup a test log message

--- container_engine_replicacontroler_test.go
+++ container_engine_replicacontroler_test.go
@@ -13,7 +13,7 @@ func TestAddExtraArgs(t *testing.T) {
 
 	for idx, val := range []string{"Sound", "Of", "the", "Train", "--Choo=Choo", "--env=\"Green=Leavesof\""} {
 		if val != run_args[idx] {
-			t.Fatalf("Failed to create run_args list correctly.  Error occured at position %d where answer key is %s and generated value is %s. Generated %q instead of Sound Of the Train --Choo Choo --env=\"Green=Leavesof\"", idx, val, run_args[idx], run_args)
+			t.Fatalf("Failed to create run_args list correctly.  Error occured at position %d where answer key is %s and generated value is %s. Generated %q instead of Sound Of the Train --Choo=Choo --env=\"Green=Leavesof\"", idx, val, run_args[idx], run_args)
 		}
 	}
 }
